f8728a9c498dfed083581a20cef957ed08aa1341
lethanhtien <113422192+GraSuki@users.noreply.github.com>
2023-07-05 15:26:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,31 @@ const express = require('express'),
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.post("/webhooks", async (req, res) => {
 
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: "lethanhtien.official@gmail.com",
+        pass: "apavxhwqlyemakmp"
+    }
+});
+
+app.post("/webhooks", async (req, res) => {
     const { body } = req;
     console.log(body)
-    res.status(200).json("Success!");
-
+    let mailOptions = {
+        from: "lethanhtien.official@gmail.com",
+        to: "lethanhtien.official@gmail.com",
+        subject: 'Sending Email using Node.js',
+        text: "Hello"
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+        } else {
+            res.status(200).json("Success!");
+        }
+    })
 });
 
 app.listen(3000);
